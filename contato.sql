404412c1c3379da1dd02878754bad9c4b676835f
Verena Aquino <aquinoverena@gmail.com>
2023-10-26 22:43:03 -0300

    inserir tres contatos telefonicos

--- contato.sql
+++ contato.sql
@@ -10,3 +10,10 @@ Id  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 Nome VARCHAR(50) NOT NULL,
 Telefone VARCHAR(11) NOT NULL
 );
+
+-- Inserir três contatos na tabela lista
+INSERT INTO lista (nome, telefone) VALUES 
+("Faustino Manuel", "987390490"),
+("Mah Aquino", "985391276"), 
+("Bru Cabezas", "936987654");
+
