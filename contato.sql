bc5e0ccedc4412d26786bcc014b926e5722fc0dd
Verena Aquino <aquinoverena@gmail.com>
2023-10-26 22:18:35 -0300

    criar tabela lista

--- contato.sql
+++ contato.sql
@@ -1,5 +1,12 @@
 -- Criar  banco de dado contato
 CREATE DATABASE contato;
 
+-- selecionar o banco de dados use contato
+USE contato;
 
-
+-- criar uma tabela chamada lista com três campos, 
+CREATE TABLE lista ( 
+Id  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+Nome VARCHAR(50) NOT NULL,
+Telefone VARCHAR(11) NOT NULL
+);
